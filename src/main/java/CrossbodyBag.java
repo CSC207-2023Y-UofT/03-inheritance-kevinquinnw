a6680368279cdfcef774dbc3431e5cd643f76291
Your Name <you@example.com>
2023-05-29 18:46:38 -0400

    spacing error

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -50,7 +50,7 @@
 
     @Override
     public String toString () {
-        return getColor() + " Crossbody Bag with " + getNumberOfStraps() + "straps" + "(" + getNumberOfContents() + " / " + getCapacity() + ")";
+        return getColor() + " Crossbody Bag with " + getNumberOfStraps() + " straps " + "(" + getNumberOfContents() + " / " + getCapacity() + ")";
     }
 
 
